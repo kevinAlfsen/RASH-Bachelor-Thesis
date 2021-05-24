940417e6ce783376d83efdf129e387acdcdb2285
kevinAlfsen <kevin.alfsen@outlook.com>
2021-05-24 22:07:18 +0200

    Stand Config

---
+++ Experiments/Stand/config.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Config:
+    # PD-settings from ZN-method
+    # kp = 4
+    # Kd = 0.0435
+
+    kp = 4
+    kd = 0.02
+
+    current_saturation = 5
+
+    interface_name = "enp4s0"
+
+    N_SLAVES_CONTROLLED = 4
+    N_SLAVES = 4
+
+    num_joints = N_SLAVES_CONTROLLED * 2
+
+    dt = 0.001
+
+    joint_limits = [[3*np.pi for i in range(N_SLAVES * 2)],[-3*np.pi for i in range(N_SLAVES * 2)]]
+
+    zero_calibration =np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.02623867 * 2,0.0]) #np.array([0.02623867,0,0,0,0,0,0,0]) 
+ 
+    k_v = [5 for i in range(8)] #max velocity per joint, output space, rad/s  #hadde 10
+    k_a = [20 for i in range(8)] #max acceleration per joint, output space, rad/s**2 #hadde 40
+
+    gear_ratio = 9
+
+    calibration_points = [
+    0, 
+    np.pi/4, 2*np.pi/4, 3*np.pi/4, 4*np.pi/4, 5*np.pi/4, 6*np.pi/4, 7*np.pi/4, 
+    8*np.pi/4, 
+    7*np.pi/4, 6*np.pi/4, 5*np.pi/4, 4*np.pi/4, 3*np.pi/4, 2*np.pi/4, np.pi/4,
+    0
+    ]
+
+    #calibration_points = [4*np.pi - i*np.pi/4 for i in range(17)]
+
+    calibration_delay = 4
+
+def Calibration_parameters(motor_index):
+    for i in range(len(Config.calibration_points)):
+        cal_params = [0,0,0,0,0,0,0,0]
+        cal_params[motor_index] = Config.calibration_points[i]
+        yield cal_params
---
+++ Experiments/Stand/interface.py
@@ -0,0 +1,259 @@
+#config.py
+#Class containing all configuration variables
+#Used to declutter modules that share config variables
+import config
+from config import Config
+
+#parser.py
+import parser
+from parser import ParseError
+
+#trajectory.py
+#Holds final and initial positions and calculates the interpolation between them
+from trajectory import Trajectory
+
+#plotter.py
+#Holds arrays with data that will be plotted as well as methods for plotting with matplotlib
+#Makes potting easier
+from plotter import Plotter
+
+#masterboard.py
+#Holds a reference to the masterboard sdk
+#Contains methods for setting the reference position of the motors and tracking it with a PD controller
+from masterboard import MasterBoard
+
+#time counter, used to count time
+from time import perf_counter
+
+#Queue is used to transfer data between threads
+#Empty is an exception thrown when trying to get from an empty queue
+from queue import Queue, Empty
+
+#Threading is used so that user input can be prompted without pausing the master board communication
+from threading import Thread, settrace #, Lock #Lock might be useful later, if GIL is not in place
+
+import numpy as np
+
+class MasterBoardTask:
+    def __init__(self, trajectory_queue):
+        #Instatiate Master Board and the plotter
+
+        self.plotter = Plotter(f"Stand config test | kp = {Config.kp}, kd = {Config.kd}, current saturation = {Config.current_saturation} A")
+
+        self.master_board = MasterBoard(self.plotter)
+
+        #Initiate global variables
+        self.trajectory_queue = trajectory_queue
+        self.trajectory = None
+
+        self.num_joints = Config.num_joints
+
+        self.encoder_readings = []
+
+        self._running = True
+        self._should_print = False
+        #Initiate plots
+
+        
+
+        for i in range(Config.num_joints):
+            self.plotter.create_plot(f"Joint {i}, Position Reference", "Position [rad]")
+            self.plotter.create_plot(f"Joint {i}, Position Reading", "Position [rad]")
+            self.plotter.create_plot(f"Joint {i}, Velocity Reference", "Velocity [rad/s]")
+            self.plotter.create_plot(f"Joint {i}, Velocity Reading", "Velocity [rad/s]")
+            self.plotter.create_plot(f"Joint {i}, Current Reference", "Current [Ampere]")
+            #self.plotter.create_plot(f"Joint {i}, Position Error", "Position Error [rad]")
+
+    def run(self):
+        print("Master board task started")
+
+        #Initiate local variables
+        t = 0
+
+        timeout = False
+
+        last = perf_counter()
+
+        q = self.master_board.get_state()[0]
+        q_prime = np.zeros(8)
+        self.master_board.set_reference(q, q_prime)
+
+        #Main loop
+        #Executes every time step untill keyboardinterrupt or master_board sets timeout = True
+        while(not timeout and self._running):
+            elapsed_time = perf_counter() - last
+
+            if (elapsed_time >= Config.dt): #Enter this if statement every timestep
+                last = perf_counter()
+                t += Config.dt
+
+                if self.trajectory != None:
+                    #Currently following a trajectory, just get the new values and send them
+                    q, q_prime = self.trajectory.step(t)
+                    self.master_board.set_reference(q, q_prime)
+
+                    position_reading, velocity_reading = self.master_board.get_state()
+
+                    #Then check if that was the last step, i.e. the trajectory is finished
+                    if self.trajectory.Done():
+                        #If the trajectory is finished, set it to None so that the next pass will fetch the next trajectory
+                        #print("Trajectory finished, waiting for next...")
+                        
+                        #self.set_points.append(q[0])
+                        self.encoder_readings.append(position_reading[0])
+                        self.trajectory = None
+
+                else:
+                    #Not currently following a trajectory, so try to fetch the next one from the queue
+                    try:
+                        self.trajectory = self.trajectory_queue.get(block=False)
+                        self.trajectory.set_initial_conditions(q, t) #Last reference sent and current time will be the intitial conditions for the trajectory
+                    except Empty:
+                        self.trajectory = None
+
+                position_reading, velocity_reading = self.master_board.get_state()
+
+                for i in range(self.num_joints):
+                    self.plotter.add_data(f"Joint {i}, Position Reference", q[i])
+                    self.plotter.add_data(f"Joint {i}, Position Reading", position_reading[i])
+                    self.plotter.add_data(f"Joint {i}, Velocity Reference", q_prime[i])
+                    self.plotter.add_data(f"Joint {i}, Velocity Reading", velocity_reading[i])
+                
+                #If a new reference was set, this will make the masterboard track that reference
+                #If not, it will keep tracking its previous reference
+                #This needs to be called every pass, even though there is no new trajectory, in case the actual position of the joints are changed, e.g. by gravity
+                timeout = self.master_board.follow_trajectory()
+
+                self.plotter.add_time(t)
+
+            #Should print is set by InputTask whenever "Position" is input to the terminal
+            #This is in an elif block, so that it doesn't increase the duration of a step
+            elif self._should_print:
+                self.print_current_position(q)
+
+        #shut down the board correctly if the while loop breaks for any reason
+        self.master_board.terminate()
+
+        print("\nENCODER READINGS\n")
+        for value in self.encoder_readings:
+            output = np.degrees(value)
+            output = str(output)
+            output = output.replace(".", ",")
+            print(output)
+        print()
+
+        for i in range(Config.num_joints // 2):
+            #self.plotter.create_axis([f"Joint {i}, Velocity Reading"])
+
+            self.plotter.create_axis([f"Joint {2*i + 1}, Position Reading", f"Joint {2*i + 1}, Position Reference"])
+            self.plotter.create_axis([f"Joint {2*i + 1}, Current Reference"])
+
+            self.plotter.create_axis([f"Joint {2 * i}, Current Reference"])
+            self.plotter.create_axis([f"Joint {2 * i}, Position Reading", f"Joint {2 * i}, Position Reference"])
+            
+
+
+            
+            #self.plotter.create_axis([f"Joint {i}, Velocity Reference"])
+            #self.plotter.create_axis([f"Joint {i}, Position Reference"])
+            self.plotter.plot()
+
+    def print_current_position(self, current_pos_ref):
+        #Prints the current reference and position of all the motors
+        #Meant to be called by the InputTask through the use of synchronized bools
+        np.set_printoptions(suppress=True)
+        print(f"\nPosition Reference: \n{np.degrees(current_pos_ref).round(5)}\n")
+        print(f"Current Position: \n{self.master_board.get_state()[0]}")
+        print(f"Position Error: \n{np.degrees(current_pos_ref.round(5) - self.master_board.get_state()[0]).round(5)}")
+        self._should_print = False
+
+    def terminate(self):
+        self._running = False
+
+    def set_print(self):
+        self._should_print = True
+
+class InputTask:
+    def __init__(self, trajectory_queue, master_board_task):
+        self.trajectory_queue = trajectory_queue
+        self.master_board_task = master_board_task
+
+        self.generated = False
+
+    def run(self):
+        print("Input task started")
+        print("[q = 1,2,3,4,5,6,7,8 or]\n[q0 = pi/2]\n\n")
+
+        #Thread is daemonic, so this loop will only stop when the main thread is stopped
+        while(True):
+            try:
+                #Waits for an input.
+                #If the input is joint angles, create a trajectory and add it to the queue
+                q_f = parser.parse_input()
+
+                trajectory = Trajectory(q_f)
+
+                if trajectory.is_valid():
+                    self.trajectory_queue.put(trajectory)
+                else:
+                    print("Trajectory not valid")
+                    #trajectory_queue.join()
+                    #self.master_board.terminate()
+
+                    #Should stop the robot where it is, but not shut down the master board
+
+                #If parser detects one of these specified inputs, the specified action will happen
+            except ParseError as e:
+                if e.msg == "Exit":
+                    self.master_board_task.terminate()
+                if e.msg == "Position":
+                    self.master_board_task.set_print()
+                if e.msg == "Calibrate":
+                    if self.generated == False:
+                        print("Calibration initated, type c to go to next point")
+                        calibration_generator = config.Calibration_parameters(0)
+                        self.generated = True
+
+                    else:
+                        try:
+                            trajectory = Trajectory(next(calibration_generator))
+                            self.trajectory_queue.put(trajectory)
+                        except StopIteration:
+                            print("Calibration Done")
+                            self.generated = False
+
+                    #for parameters in config.Calibration_parameters(0):
+                    #    trajectory = Trajectory(parameters, Config.clibration_delay)
+                    #    self.trajectory_queue.put(trajectory)
+
+#Locked object to communicate between threads
+#As long as the GIL is implented, this is not needed ?
+#
+#class SharedFlags:
+#    def __init__(self):
+#        self.should_print = False
+#        self.flag_lock = Lock()
+#
+#    def set_print(self):
+#        with self.flag_lock:
+#            self.should_print = True
+#
+#    def get_print(self):
+#        with self.flag_lock:
+#            return self.should_print
+
+trajectory_queue = Queue()
+
+master_board_task = MasterBoardTask(trajectory_queue)
+
+
+
+input_task = InputTask(trajectory_queue, master_board_task)
+input_thread = Thread(target=input_task.run, daemon=True)
+input_thread.start()
+
+master_board_task.run()
+
+exit()
+
+
